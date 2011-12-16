f01fe4e9c33933a93052f39054990ae4caec4156
probinso <probinso@localhost.localdomain>
2011-12-15 20:52:57 -0800

    GraphBoard.java

---
+++ GraphBoard.java
@@ -0,0 +1,20 @@
+public class GraphBoard extends GameBoard
+{
+    /*
+      The purpose of this class is to abstract 
+      boards to have traditional graphing tools      
+     */
+
+    /*
+      + Mimimun Spanning Tree with implied connections
+      
+      + Identify Sink Nodes Given Board
+
+      + Identify Sink Nodes Given Minimum Spanning Tree
+
+      + Shortest Valid Path from Point to End
+
+      + Shortest Valid Path from Point to Point
+      
+     */
+}
